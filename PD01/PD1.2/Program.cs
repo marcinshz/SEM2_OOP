b5a00a874da57eb0e9fcc8f29e8df50abcdfd702
Marcin Chodkowski <marcinshaz4m@gmail.com>
2021-04-16 17:35:31 +0200

    Homework 01

---
+++ PD01/PD1.2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+/*Zadanie 5 (do domu)
+Napisać prostą klasę symulującą prosty kalkulator (wystarczą cztery działania i możliwość zapisu wyniku do pamięci).*/
+namespace PD1._2
+{
+    class Kalkulator
+    {
+        public double wynik;
+        
+        public double Dodaj(double x, double y)
+        {
+            this.wynik = x + y;
+            return x + y;
+        }
+        public double Odejmij(double x, double y)
+        {
+            this.wynik = x - y;
+            return x - y;
+        }
+        public double Pomnoz(double x, double y)
+        {
+            this.wynik = x * y;
+            return x * y;
+        }
+        public double PodniesDoPotegi(double x, int potega)
+        {
+            double wynik = x;
+            for (int i = 1; i < potega; i++)
+            {
+                wynik = wynik * x;
+            }
+            this.wynik = wynik;
+            return wynik;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kalkulator k1 = new Kalkulator();
+            Console.WriteLine(k1.PodniesDoPotegi(3, 4));
+        }
+    }
+}
---
+++ PD01/PD1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+namespace PD1
+{
+    class Zespolona
+    {
+        public double x;
+        public double y;
+
+        public Zespolona(double czRzeczywista, double czUrojona)
+        {
+            this.x = czRzeczywista;
+            this.y = czUrojona;
+        }
+        public void WyswietlWspolrzedne()
+        {
+            Console.WriteLine($"({x};{y})");
+        }
+        //a - cz. rzeczywista liczby zespolonej przez którą mnożymy
+        //b - cz. urojona liczby zespolonej przez którą mnożymy
+        public void Dodaj(double a, double b)
+        {
+            this.x += a;
+            this.y += b;
+        }
+        public void Odejmij(double a, double b)
+        {
+            this.x -= a;
+            this.y -= b;
+        }
+        public void Pomnoz(double a, double b)
+        {
+            double tmpx = x;
+            double tmpy = y;
+            this.x = ((tmpx * a) - (tmpy * b));
+            this.y = ((tmpx * b) + (tmpy * a));
+        }
+        public void Podziel(double a, double b)
+        {
+            double tmpx = x;
+            double tmpy = y;
+            this.x = ((tmpx * a + tmpy * b) / ((a * a) + (b * b)));
+            this.y = ((a * tmpy - b * tmpx) / ((a * a) + (b * b)));
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Zespolona z1 = new Zespolona(2, 3);
+            z1.WyswietlWspolrzedne();
+            z1.Pomnoz(5, 6);
+            z1.WyswietlWspolrzedne();
+            z1.Podziel(5, 6);
+            z1.WyswietlWspolrzedne();
+        }
+    }
+}
