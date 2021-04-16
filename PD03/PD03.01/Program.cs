df67a6efc57140dbd7e31fa64ed6b878e63e6b57
Marcin Chodkowski <marcinshaz4m@gmail.com>
2021-04-16 17:36:06 +0200

    Homework 03

---
+++ PD03/PD03.01/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace PD03._01
+{
+    class Pies
+    {
+        public string imie;
+        public double wiek;
+        protected string siersc = "krótka";
+        protected string rasa = "kundel";
+
+        public Pies(string imie, double wiek, string siersc, string rasa)
+        {
+            this.imie = imie;
+            this.wiek = wiek;
+            this.siersc = siersc;
+            this.rasa = rasa;
+        }
+        public Pies(string imie, double wiek)
+        {
+            this.imie = imie;
+            this.wiek = wiek;
+        }
+
+        public void Wyswietl()
+        {
+            Console.WriteLine($"Imię: {imie}, Rasa: {rasa}, Sierść: {siersc}, Wiek: {wiek}");
+        }
+    }
+    class Wezel
+    {
+        public Wezel nastepny;
+        public string nagroda;
+    }
+    class ListaNagrod
+    {
+        public Wezel glowa;
+
+        public void Dodaj(string nagroda)
+        {
+            Wezel w = new Wezel();
+            w.nagroda = nagroda;
+            if (glowa == null)
+            {
+                glowa = w;
+            }
+            else
+            {
+                for (Wezel tmp = glowa; tmp != null; tmp = tmp.nastepny)
+                {
+                    if (tmp.nastepny == null)
+                    {
+                        tmp.nastepny = w;
+                    }
+                }
+            }
+        }
+    }
+    class PiesRasowy : Pies
+    {
+        public ListaNagrod listanagrod;
+
+        public PiesRasowy(string imie, double wiek, string siersc, string rasa) : base(imie, wiek, siersc, rasa)
+        {
+            this.imie = imie;
+            this.wiek = wiek;
+            this.siersc = siersc;
+            this.rasa = rasa;
+        }
+
+        public void DodajNagrode(string nagroda)
+        {
+            listanagrod.Dodaj(nagroda);
+        }
+    }
+    class Chart : PiesRasowy
+    {
+        public double predkosc=0;
+
+        public Chart(double predkosc, string imie, double wiek, string siersc, string rasa) : base(imie, wiek, siersc, rasa)
+        {
+            this.imie = imie;
+            this.wiek = wiek;
+            this.siersc = siersc;
+            this.rasa = rasa;
+            this.predkosc = predkosc;
+        }
+
+        public void Biegnij()
+        {
+            predkosc = 15;
+        }
+        public void Stoj()
+        {
+            predkosc = 0;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ PD03/PD03.02/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace PD03._02
+{
+    class Osoba
+    {
+        public string imie;
+        public string nazwisko;
+        public int rokUrodzenia;
+        public string plec;
+
+        public Osoba(string imie, string nazwisko, int rokUrodzenia, string plec)
+        {
+            this.imie = imie;
+            this.nazwisko = nazwisko;
+            this.rokUrodzenia = rokUrodzenia;
+            this.plec = plec;
+        }
+    }
+    class Wykladowca : Osoba
+    {
+        public string tytulNaukowy;
+        public Wykladowca(string imie, string nazwisko, int rokUrodzenia, string plec, string tytulNaukowy) : base(imie, nazwisko, rokUrodzenia, plec)
+        {
+            this.imie = imie;
+            this.nazwisko = nazwisko;
+            this.rokUrodzenia = rokUrodzenia;
+            this.plec = plec;
+            this.tytulNaukowy = tytulNaukowy;
+        }
+    }
+    class Student : Osoba
+    {
+        public int numerIndeksu;
+        public Student(string imie, string nazwisko, int rokUrodzenia, string plec, int numerIndeksu) : base(imie, nazwisko, rokUrodzenia, plec)
+        {
+            this.imie = imie;
+            this.nazwisko = nazwisko;
+            this.rokUrodzenia = rokUrodzenia;
+            this.plec = plec;
+            this.numerIndeksu = numerIndeksu;
+        }
+    }
+    class Stypendysta : Student
+    {
+        public double kwotaStypendium;
+        public Stypendysta(string imie, string nazwisko, int rokUrodzenia, string plec, int numerIndeksu, int kwotaStypendium) : base(imie, nazwisko, rokUrodzenia, plec, numerIndeksu)
+        {
+            this.imie = imie;
+            this.nazwisko = nazwisko;
+            this.rokUrodzenia = rokUrodzenia;
+            this.plec = plec;
+            this.numerIndeksu = numerIndeksu;
+            this.kwotaStypendium = kwotaStypendium;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
